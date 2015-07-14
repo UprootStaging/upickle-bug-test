99d7114ef887df500ea71b7a29d84dffa020f3e7
hrj <harshad.rj@gmail.com>
2015-07-14 22:27:16 +0530

    removed irrelevant files

--- uni/jvm/src/main/scala/uni/UniServer.scala
+++
@@ -1,8 +0,0 @@
-package uni
-
-object UniServer extends Api {
-  def getForm(id: String, recordId: String): (Form, Map[String, String]) = {
-    null
-  }
-
-}
--- uni/shared/src/main/scala/Shared.scala
+++
@@ -1,7 +0,0 @@
-package uni
-
-trait Api{
-  def getForm(id: String, recordId: String): (Form, Map[String, String])
-
-}
-
